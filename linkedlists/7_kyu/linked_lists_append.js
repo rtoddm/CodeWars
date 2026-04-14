b4eb2ae41108ac636010dcfe5229040c2cf8ad03
ToddM <rtoddm@gmail.com>
2026-04-14 19:39:01 -0400

    add function to append one linked list to another

---
+++ linkedlists/7_kyu/linked_lists_append.js
@@ -0,0 +1,32 @@
+// LINKED LISTS APPEND
+
+// Link to original problem
+// https://www.codewars.com/kata/55d17ddd6d7868493e000074/train/javascript
+
+// Instructions
+// Write an Append() function which appends one linked list to another. The head of the resulting list should be returned.
+
+// var listA = 1 -> 2 -> 3 -> null
+// var listB = 4 -> 5 -> 6 -> null
+// append(listA, listB) === 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> null
+// If both listA and listB are null/NULL/None/nil, return null/NULL/None/nil. If one list is null/NULL/None/nil and the other is not, simply return the non-null/NULL/None/nil list.
+
+// Solution
+function Node(data) {
+  this.data = data;
+  this.next = null;
+}
+
+function append(listA, listB) {
+  let current = listA;
+
+  if (listA === null) return listB;
+  if (listB === null) return listA;
+
+  while (current.next != null) {
+    current = current.next;
+  }
+
+  current.next = listB;
+  return listA;
+}
