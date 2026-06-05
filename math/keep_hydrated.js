ef47c0a5140ff04be5f36ab9ea657eadf7829a77
ToddM <rtoddm@gmail.com>
2026-06-04 19:55:35 -0500

    add solution for "Keep Hydrated" problem

---
+++ math/keep_hydrated.js
@@ -0,0 +1,22 @@
+// KEEP HYDRATED
+
+// Link to Original Problem
+// https://www.codewars.com/kata/582cb0224e56e068d800003c/train/javascript
+
+// Instructions
+// Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+
+// You get given the time in hours and you need to return the number of litres Nathan will drink, rounded down.
+
+// For example:
+
+// time = 3 ----> litres = 1
+
+// time = 6.7---> litres = 3
+
+// time = 11.8--> litres = 5
+
+// Solution
+function litres(time) {
+  return Math.floor(0.5 * time);
+}
